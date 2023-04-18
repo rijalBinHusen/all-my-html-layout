2646a09a34bb2a0c8a302be2626522602aa41e81
rijalBinHusen <jeryzal333@gmail.com>
2023-04-18 08:36:53 +0700

    add ketupat

--- idulfitri/main.js
+++ idulfitri/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from "controls";
+import { GLTFLoader } from "loader"
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -10,28 +11,44 @@ document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
 // camera.position.x = 4;
 // camera.position.y = -5;
 
-const ambientlight = new THREE.AmbientLight( 0xffffff,  0.5); // soft white light
+const ambientlight = new THREE.AmbientLight( 0xffffff,  10); // soft white light
 scene.add( ambientlight );
 
 const pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
 pointLight.position.set( 50, 50, 50 );
 scene.add( pointLight );
 
+const loader = new GLTFLoader();
+let ketupat;
+
+loader.load( 'ketupat/scene.gltf', function ( gltf ) {
+    ketupat = gltf.scene
+    ketupat.scale.set(30, 30, 30)
+	scene.add( ketupat );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.z  += 0.01;
+    if(ketupat){
+        ketupat.rotation.y  += 0.01;
+    }
 	renderer.render( scene, camera );
-    controls.update()
+    // controls.update()
 }
 
 animate();
