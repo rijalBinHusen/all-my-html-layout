9131acb12813ccb2c4632ed0fdb7403a4526629e
rijalBinHusen <jeryzal333@gmail.com>
2024-11-21 14:39:23 +0700

    E-kpi oke

--- javascript/e-kpi/fetch_report.ts
+++ javascript/e-kpi/fetch_report.ts
@@ -74,7 +74,7 @@ function logout() {
     return fetch(location.origin + "/KPI/auth/logout")
 }
 
-async function insert_kpi(user_id: string, periode1: string, periode2: string) {
+async function insert_kpi(user_id: string, periode1: string, periode2: string, kuantitatifPoint: pointKPI[], kualitatifPoint: pointKPI[]) {
 
     const response_report = await fetch(location.origin + `/KPI/raport/detail_raport?id=${user_id}&dept=7&raport=&tgl1=${periode1}&tgl2=${periode2}`, {
         "headers": {
@@ -106,23 +106,25 @@ async function insert_kpi(user_id: string, periode1: string, periode2: string) {
     // add kuantitatif
     for(let [index, kuan] of report.kuanti.entries()) {
         formData.append(`target1[${index}]`, '100');
-        formData.append(`real1[${index}]`, '100');
+        formData.append(`real1[${index}]`, kuantitatifPoint[index].points + '');
         formData.append(`id1[${index}]`, '');
         formData.append(`indikator1[${index}]`, kuan.indikator);
         formData.append(`params1[${index}]`, kuan.params);
         formData.append(`bobot1[${index}]`, kuan.bobot);
-        formData.append(`ach1[${index}]`, '100');
+        formData.append(`ach1[${index}]`, kuantitatifPoint[index].points + '');
     }
 
+    let index = 0;
     // add kualitatif
     for(let kual of report.kuali) {
         formData.append(`target2[]`, '100');
-        formData.append(`real2[]`, '100');
+        formData.append(`real2[]`, kualitatifPoint[index].points + '');
         formData.append(`id2[]`, '');
         formData.append(`desk2[]`, kual.desk);
         formData.append(`params2[]`, kual.params);
         formData.append(`bobot2[]`, kual.bobot);
-        formData.append(`ach2[]`, '100');
+        formData.append(`ach2[]`, kualitatifPoint[index].points + '');
+        index++;
     }
 
     formData.append("document[]", "(binary)");
@@ -132,51 +134,27 @@ async function insert_kpi(user_id: string, periode1: string, periode2: string) {
     formData.append("tgl_awal", periode1);
     formData.append("tgl_akhir", periode2);
     formData.append("nama", report.nama);
-                
-    await fetch(location.origin + "/KPI/raport/edit_raport", {
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "max-age=0",
-            "upgrade-insecure-requests": "1"
-        },
-        "referrer": location.origin + "/KPI/raport",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": formData,
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-    });
+    
+
+    for (const [key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`);
+      }
+    // await fetch(location.origin + "/KPI/raport/edit_raport", {
+    //     "headers": {
+    //         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    //         "accept-language": "en-US,en;q=0.9",
+    //         "cache-control": "max-age=0",
+    //         "upgrade-insecure-requests": "1"
+    //     },
+    //     "referrer": location.origin + "/KPI/raport",
+    //     "referrerPolicy": "strict-origin-when-cross-origin",
+    //     "body": formData,
+    //     "method": "POST",
+    //     "mode": "cors",
+    //     "credentials": "include"
+    // });
 
     return true
-}
-
-
-async function start_insert(users: Users_and_details[], periode1: string, periode2: string) {
-    
-    let timer = 700;
-    let counter = 1;
-
-    for(let user of users) {
-        
-        timer += 12;
-        await new Promise(resolve => setTimeout(resolve, timer));
-        if (timer >= 5000) timer = 650;
-
-        console.log(`Melakukan input raport ${user.name}, data ke ${counter++} dari ${users.length}`);
-
-        // login
-        const isLoginSuccess = await login(user.username, user.password);
-        if(!isLoginSuccess) {
-            console.log(`Gagal login ${user.name}`);
-            continue;
-        }
-
-        // insert kpi
-        await insert_kpi(user.id_user, periode1, periode2)
-        // logout
-        await logout();
-    }
 }
 
 interface spreadsheetReponse {
@@ -212,14 +190,135 @@ async function getUsersEKPI(): Promise<Users_and_details[]|false> {
     return false
 }
 
+interface pointKPI {
+    points: number,
+    KRA:  string
+}
+
+interface pointsEKPI {
+    [username: string]: {
+        kuali: pointKPI[],
+        kuanti: pointKPI[]
+    }
+
+}
+
+async function getPointEKPI(): Promise<pointsEKPI|false> {
+    const pointAlphabet = {
+        B: 100,
+        C: 80,
+        K: 60
+    }
+    // Construct the URL for the Google Sheets API request
+    const urlKuanti = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetIdEKPI}/values/${rangeKuantiEKPI}?key=${spreadsheetAPIKey}`;
+    // Fetch the data from the spreadsheet
+    const getData = await fetch(urlKuanti);
+    const kuantitatifPoint = await getData.json() as spreadsheetReponse;
+    const pointsEKPI:pointsEKPI = {};
+    
+    if(kuantitatifPoint?.values && kuantitatifPoint.values.length) {
+        for(let i = 0; i < kuantitatifPoint.values.length; i++) {
+            const kuan = kuantitatifPoint.values[i];
+            const kuantiUsername = kuan[0];
+            const kuantiParam = kuan[8];
+            const KRA =  kuan[2];
+            // if point is exists
+            if(kuantiParam && Object.keys(pointAlphabet).includes(kuantiParam)) {
+                const isUsernameInitialized = pointsEKPI && pointsEKPI[kuantiUsername];
+                
+                if(isUsernameInitialized) {
+                    pointsEKPI[kuantiUsername]['kuanti'].push({
+                        points: pointAlphabet[kuantiParam],
+                        KRA
+                    })
+                }
+                else {
+                    pointsEKPI[kuantiUsername] = {
+                            kuanti: [{
+                                points: pointAlphabet[kuantiParam],
+                                KRA
+                            }],
+                            kuali: []
+                    }
+                }
+            }
+        }
+    }
+
+    
+    const urlKuali = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetIdEKPI}/values/${rangeKualiEKPI}?key=${spreadsheetAPIKey}`;
+    // Fetch the data from the spreadsheet
+    const getKuali = await fetch(urlKuali);
+    const kualitatifPoint = await getKuali.json() as spreadsheetReponse;
+
+    if(kualitatifPoint?.values && kualitatifPoint.values.length) {
+        for(let i = 0; i < kualitatifPoint.values.length; i++) {
+            const kual = kualitatifPoint.values[i];
+            const kualiUsername = kual[0];
+            const kualiKRA = kual[2];
+            const kualiPoint = Number(kual[6]) < 60 ? 0 : Number(kual[6]);
+            // if point is exists
+            if(kualiKRA) {
+                const isUsernameInitialized = pointsEKPI && pointsEKPI[kualiUsername];
+
+                if(isUsernameInitialized) {
+                    pointsEKPI[kualiUsername]['kuali'].push({
+                        points: kualiPoint,
+                        KRA: kualiKRA
+                    })
+                }
+            }
+        }
+    }
+
+    return pointsEKPI
+}
+
 async function startProcess() {
-    const getUsers = await getUsersEKPI();
-    if(!getUsers) {
+    const users = await getUsersEKPI();
+    if(!users) {
         console.log("Gagal mendapatkan users");
         return;
     }
 
-    start_insert(getUsers, "2024-10-27", "2024-11-02")
+     const periode1 = "2024-10-27";
+     const periode2 = "2024-11-02";
+    
+    let timer = 700;
+    let counter = 1;
+
+    for(let user of users) {
+        if(!user.id_user || !user.name || !user.password || !user.username) continue;
+        timer += 12;
+        await new Promise(resolve => setTimeout(resolve, timer));
+        if (timer >= 5000) timer = 650;
+
+        console.log(`Melakukan input raport ${user.name}, data ke ${counter++} dari ${users.length}`);
+
+        // login
+        const isLoginSuccess = await login(user.username, user.password);
+        if(!isLoginSuccess) {
+            console.log(`Gagal login ${user.name}`);
+            continue;
+        }
+
+        const pointsKPI = await getPointEKPI();
+        if(!pointsKPI) {
+            console.log(`Gagal melakukan input E-KPI karena semua point E-KPI tidak ditemukan`);
+            return
+        }
+        
+        const isEKPIPointExists = pointsKPI[user.name];
+        
+        if(!isEKPIPointExists) {
+            console.log(`Gagal melakukan input E-KPI ${user.name} karena nilai tidak ditemukan`);
+            return;
+        }
+        // insert kpi
+        await insert_kpi(user.id_user, periode1, periode2, pointsKPI[user.name].kuanti, pointsKPI[user.name].kuali);
+        // logout
+        await logout();
+    }
 }
 
 startProcess()
